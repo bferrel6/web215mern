7fcc35e7069e239602fc019d4cf56742735d3cb2
bferrel6 <116413200+bferrel6@users.noreply.github.com>
2024-03-16 14:56:12 -0400

    Update DeleteRecord.jsx
    update color of delete page border

--- frontend/src/pages/DeleteRecord.jsx
+++ frontend/src/pages/DeleteRecord.jsx
@@ -33,7 +33,7 @@ const DeleteRecord = () => {
       <BackButton />
       <h2 className='text-3xl my-4'>Delete Record</h2>
       {loading ? <Spinner /> : ''}
-      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
+      <div className='flex flex-col items-center border-2 border-green-200 rounded-xl w-[600px] p-8 mx-auto'>
         <h3 className='text-2xl'>Are you <em>sure</em> you want to delete this record?</h3>
         <button
           className='p-4 bg-red-600 text-white m-8 w-full'
