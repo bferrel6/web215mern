3828d28f19e13b927455097bbee0980e4dcc485e
bferrel6 <116413200+bferrel6@users.noreply.github.com>
2024-03-19 22:00:05 -0400

    Update config.js
    file name bug fix attempt

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 
+export const localMongoDBURL = 
     "mongodb+srv://bferrel6:TKIT3GhFmJgYdKbC@mern-music-store.7otix9v.mongodb.net/record-collection?retryWrites=true&w=majority&appName=mern-music-store"
 
 export const remoteMongoDBURL = 
