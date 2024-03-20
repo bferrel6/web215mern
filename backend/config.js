1fbd5dbeaa697c43b84e88f6f5448ab5d5130314
bferrel6 <116413200+bferrel6@users.noreply.github.com>
2024-03-19 21:56:51 -0400

    Update config.js
    database address fix attempt

--- backend/config.js
+++ backend/config.js
@@ -1,14 +1,7 @@
 export const PORT = 5555;
 
-export const localMongoDBURL = 
+export const mongoDBURL = 
     "mongodb+srv://bferrel6:TKIT3GhFmJgYdKbC@mern-music-store.7otix9v.mongodb.net/record-collection?retryWrites=true&w=majority&appName=mern-music-store"
 
-const atlasAddresses = [
-    '3.129.111.220',
-    '3.134.238.10',
-    '52.15.118.168'
-]
-const randomAtlasIP = atlasAddresses[Math.floor(Math.random() * atlasAddresses.length)];
-
 export const remoteMongoDBURL = 
-    `mongodb://bferrel6:TKIT3GhFmJgYdKbC@${randomAtlasIP}/record-collection?retryWrites=true&w=majority&appName=mern-music-store`
+    `mongodb://bferrel6:TKIT3GhFmJgYdKbC@mern-music-store.7otix9v.mongodb.net/record-collection?retryWrites=true&w=majority&appName=mern-music-store`
