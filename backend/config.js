d3c6dc6ec4a471d104cc0d5db4925cce84aac41e
bferrel6 <116413200+bferrel6@users.noreply.github.com>
2024-03-19 22:07:17 -0400

    Update config.js
    mongodb address fix attempt

--- backend/config.js
+++ backend/config.js
@@ -4,4 +4,4 @@ export const localMongoDBURL =
     "mongodb+srv://bferrel6:TKIT3GhFmJgYdKbC@mern-music-store.7otix9v.mongodb.net/record-collection?retryWrites=true&w=majority&appName=mern-music-store"
 
 export const remoteMongoDBURL = 
-    `mongodb://bferrel6:TKIT3GhFmJgYdKbC@mern-music-store.7otix9v.mongodb.net/record-collection?retryWrites=true&w=majority&appName=mern-music-store`
+    `mongodb+srv://bferrel6:TKIT3GhFmJgYdKbC@mern-music-store.7otix9v.mongodb.net/record-collection?retryWrites=true&w=majority&appName=mern-music-store`
